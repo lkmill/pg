4719bd44d9d87e8eb3ae4873f6e2a38799aa2411
Linus Miller <lohfu@lohfu.io>
2017-04-29 21:58:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const pg = require('pg');
+
+function transaction(client) {
+  return {
+    commit() {
+      return client.query('COMMIT;').then(() => {
+        client.release();
+      }).catch((err) => this.rollback().then(() => {
+        throw err;
+      }));
+    },
+
+    rollback() {
+      return client.query('ROLLBACK;').then(() => {
+        // if there was a problem rolling back the query
+        // something is seriously messed up.  Return the error
+        // to the done function to close & remove this client from
+        // the pool.  If you leave a client in the pool with an unaborted
+        // transaction weird, hard to diagnose problems might happen.
+
+        client.release();
+      });
+    },
+
+    query(...args) {
+      if (!args[0]) throw new Error('You need to supply a string as a query');
+
+      return client.query(...args)
+        .catch((err) => this.rollback().then(() => {
+          throw err;
+        }));
+    },
+  };
+}
+
+module.exports = (conf) => {
+  const pool = (conf instanceof pg.Pool) ? conf : new pg.Pool(conf);
+
+  return {
+    end(...args) {
+      return pool.end(...args);
+    },
+
+    query(...args) {
+      if (!args[0]) throw new Error('You need to supply a string as a query');
+
+      return pool.query(...args);
+    },
+
+    connect(...args) {
+      return pool.connect(...args);
+    },
+
+    begin() {
+      return pool.connect().then((client) => {
+        const t = transaction(client);
+
+        return t.query('BEGIN;')
+          .then(() => t)
+          .catch((err) => t.rollback().then(() => {
+            throw err;
+          }));
+      });
+    },
+  };
+};
